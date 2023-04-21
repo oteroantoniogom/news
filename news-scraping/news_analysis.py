bef3cc83a0af2e8af04116270480ceffbe0d1efb
oteroantoniogom <oteroantoniogomez@gmail.com>
2023-04-21 14:19:26 +0200

    pequeños cambios

--- news-scraping/news_analysis.py
+++ news-scraping/news_analysis.py
@@ -30,5 +30,5 @@ for title in newspapers:
     df = pd.DataFrame(lista, columns=["URL", "CONTENIDO"])
     # Guardar el dataframe como un archivo de Excel en la raíz del proyecto con el nombre del dominio del periódico
     df.to_excel(f"{dominio}.xlsx", index=False, encoding="utf-8")
-    print(f"{dominio} analizado.")
+    print(f"{dominio} analizado")
 print("Análisis finalizado")
