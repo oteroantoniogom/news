095a7321043bfda0b573b4d7edbad50f29d166c6
oteroantoniogom <oteroantoniogomez@gmail.com>
2023-04-21 13:39:59 +0200

    Añadido .xlsx a .gitignore

--- news-scraping/noticias.py
+++ news-scraping/noticias.py
@@ -6,9 +6,6 @@ user_agent = UserAgent()
 
 newspapers = ["https://es.cointelegraph.com/", "https://www.elmundo.es/", "https://www.larazon.es/"]
 
-# Definir dataframe vacío
-#df = pd.DataFrame(columns=["URL", "CONTENIDO"])
-
 # Recorrer cada periódico en la lista de periódicos
 for title in newspapers:
     lista = []
@@ -21,16 +18,12 @@ for title in newspapers:
             article.parse()
             # Agregar la URL y el contenido del artículo al dataframeim
             data = [article.url, article.text]
-            #df = df.append({"URL":article.url, "CONTENIDO":article.text}, ignore_index=True)
-            #df = df.append([article.url, article.text], ignore_index=True)
             lista.append(data)
         except Exception as ex:
-            # Se produce un error
+            # Se produce un error al descargar o analizar el artículo
             print(f"Error al descargar o analizar el artículo {article.url}: {str(ex)}")
     dominio = title.split(".")[1]
     print(dominio)
     print(title)
     df = pd.DataFrame(lista, columns=["URL", "CONTENIDO"])
     df.to_excel(f"{dominio}.xlsx", index=False, encoding="utf-8")
-
-df.to_csv("noticias.csv", index=False, encoding="utf-8")
